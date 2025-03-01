5e6ba951962e8acc1652aff96c006ba8c242bab4
Amitay Auerbach <amitayauerbach@Amitays-MacBook-Pro.local>
2025-03-01 13:35:45 +0200

    v20

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'amitayay1@gmail.com'  # Replace with your email
+app.config['MAIL_USERNAME'] = 'amitayab@gmail.com'  # Replace with your email
 app.config['MAIL_PASSWORD'] = 'dbzs zmrx zcod gicl'  # Replace with your app password
-app.config['MAIL_DEFAULT_SENDER'] = 'amitayay1@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'amitayab@gmail.com'
 
 mail = Mail(app)
 
